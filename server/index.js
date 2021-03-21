cfcb9fb7e86385b4c9ed7bfd3576e73c450de4c7
KrystynaMil <kristina-manager@hotmail.com>
2021-03-21 21:52:38 +0100

    final touches

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -7,19 +8,17 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-
-app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
-
 
 app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://events:events12345@cluster0.chbox.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-.catch((error) => console.log(error.message));
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -2,20 +2,20 @@ import mongoose from 'mongoose';
 
 const postSchema = mongoose.Schema({
     title: String,
-     message: String,
-     creator: String,
-     tags: [String],
-     selectedFile: String,
-     likeCount: {
-         type: Number,
-         default: 0
-     },
-     createdAt: {
-         type: Date,
-         default: new Date()
-     },
-});
+    message: String,
+    creator: String,
+    tags: [String],
+    selectedFile: String,
+    likeCount: {
+        type: Number,
+        default: 0,
+    },
+    createdAt: {
+        type: Date,
+        default: new Date(),
+    },
+})
 
-const PostMessage = mongoose.model('PostMessage', postSchema);
+var PostMessage = mongoose.model('PostMessage', postSchema);
 
-export default PostMessage
+export default PostMessage;
