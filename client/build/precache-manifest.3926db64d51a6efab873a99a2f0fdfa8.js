c3db4ca6308bb3e9dbf8b045b1b922649f2c79a6
KrystynaMil <kristina-manager@hotmail.com>
2021-04-07 17:12:30 +0200

    client sign-in button

---
+++ client/build/precache-manifest.3926db64d51a6efab873a99a2f0fdfa8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c02787702f661343a3df8d2e505e34be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e32025743cf6677cb4fc",
+    "url": "/static/css/main.34328e42.chunk.css"
+  },
+  {
+    "revision": "930d0be7ca1353ef2eb7",
+    "url": "/static/js/2.fe963d16.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.fe963d16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e32025743cf6677cb4fc",
+    "url": "/static/js/main.590ed939.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "cf18dd449f5f94fcd2abc813c62c83e7",
+    "url": "/static/media/events.cf18dd44.png"
+  }
+]);
